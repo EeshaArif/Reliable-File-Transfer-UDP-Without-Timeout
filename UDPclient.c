3e77fa70bf44ba39917e55531a513401b224200b
EeshaArif <eeshaarif@gmail.com>
2020-03-27 04:58:30 +0500

    main-logic

--- UDPclient.c
+++ UDPclient.c
@@ -10,8 +10,8 @@
 # include <sys/stat.h>
 # include <sys/sendfile.h>
 # include <fcntl.h>
+# include <time.h>
 
-# define PORT 15000
 # define ADDRESS "127.0.0.1"
 # define videoFile "earth.mov"
 # define BUFSIZE 500 // Restricting payload 
@@ -22,21 +22,41 @@ struct packet {
 	int size;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+
+	// Two arguments should be provided
+	if (argc < 2) {
+		perror("Port Number not specified.\nTry ./cli.out 9898\n");
+                return 0;
+
+	}
+
+	// Converting string to integer (atoi)
+	int PORT = atoi(argv[1]);
 	int _socket;
 	struct sockaddr_in address;
-	socklen_t addr_length = sizeof(address);
+	socklen_t addr_length = sizeof(struct sockaddr_in);
 
+	struct timespec time1, time2;
+	time1.tv_sec = 0;
+	time1.tv_nsec = 500000000L;
 
 	int sendlen;
+	int len;
+	int recvlen;
 	int file;
+        int length = 5;
 	struct stat fileStat;
 	off_t fileSize;
 
+	struct packet packets[5]; // Restricting to 5 UDP segments 
+	int _seqNum = 1;
+	int _acks;
+	int totalPackets = 0;
+	struct packet ack;
+	struct packet acks[5];
+        char end[BUFSIZE] = "END OF FILE";
 
-
-
-	//char* message = "taddaaaa, it worked";
 
 
 	// Socket Created
@@ -71,8 +91,8 @@ int main(int argc, char** argv) {
 
 	// Calculating the size of the video file
 	fstat(file, &fileStat);
-	fileSize = file_stat.st_size;
-	fprintf(stdout, "Size of Video File: %d bytes\n", fileSize);
+	fileSize = fileStat.st_size;
+	fprintf(stdout, "Size of Video File: %d bytes\n",(int) fileSize);
 	
 
 	// Sending the size of the file to the server
@@ -83,6 +103,71 @@ int main(int argc, char** argv) {
 		goto FILESIZE;
 	}
 
+	len = 1;
+
+	while (len > 0) {
+
+
+		// Putting file data into packets
+		_seqNum = 0;
+		for (int i = 0; i < length; i++) {
+
+			len = read(file, packets[i].data, BUFSIZE);
+			packets[i].seqNum = _seqNum;
+			packets[i].size = len;
+			_seqNum++;
+			// The last packet to be sent
+                        if (len == 0){ printf("End of file reached.\n");packets[i].size = -999; length = i + 1; break; }
+
+
+
+		}
+
+		// Sending Packets 
+		for (int i = 0; i < length; i++) {
+			fprintf(stdout, "Sending packet %d\n", packets[i].seqNum);
+			sendlen = sendto(_socket, &packets[i], sizeof(struct packet), 0, (struct sockaddr*) & address, addr_length);
+		}
+
+
+		// Setting array of Acks to zero 
+		memset(acks, 0, sizeof(acks));
+                for (int i = 0; i < length; i++){ acks[i].size = 0;}
+
+		// stop and wait after sending 5 packets
+		// code is sleeping for 1s + 500ns
+		//nanosleep(&time1, &time2);
+		_acks = 0;
+		// receiving Acknowledgments
+		for (int i = 0; i < length; i++) {
+			recvlen = recvfrom(_socket, &ack, sizeof(struct packet), 0, (struct sockaddr*) & address, &addr_length);
+			if (ack.size == -99) {
+				fprintf(stdout, "Ack Received: %d\n", ack.seqNum);
+				acks[ack.seqNum] = ack;
+				_acks++;
+			}
+                     
+		}
+
+		// selective Repeat 
+		// sending those packets whose acks have not been received
+		for (int i = 0; i < length; i++) {
+			if (acks[i].size == 0) {
+				sendlen = sendto(_socket, &packets[i], sizeof(struct packet), 0, (struct sockaddr*) & address, addr_length);
+			}
+		}
+
+
+
+
+		
+
+
+
+	}
+
+
+	
 
 
 
@@ -97,6 +182,9 @@ int main(int argc, char** argv) {
 
 
 
+
+
+	close(_socket);
 
 	return 0;
 }
--- UDPserver.c
+++ UDPserver.c
@@ -9,7 +9,7 @@
 # include <netinet/in.h>
 # include <fcntl.h>
 
-# define PORT 15000
+
 # define recVideoFile "received-video.mov"
 # define BUFSIZE 500 // Restricting payload 
 
@@ -19,15 +19,34 @@ struct packet {
 	int size;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+	// Two arguments should be provided
+	if (argc < 2) {
+		perror("Port Number not specified. \nTry ./serv.out 9898\n");
+                return 0;
+
+	}
+
+	// Converting string to integer (atoi)
+	int PORT = atoi(argv[1]);
 	int _bind;
 	int _socket;
 	struct sockaddr_in address;
-	socklen_t addr_length = sizeof(address);
+	socklen_t addr_length;
+        addr_length = sizeof(struct sockaddr_in);
 
 	int recvlen;
+	int sendlen;
 	int file;
 	int fileSize;
+	int remainingData;
+        int receivedData;
+        int length = 5;
+	
+	struct packet _packet;
+	struct packet packets[5];
+	int _acks;
+	struct packet acks[5];
 
 
 	// Socket Created
@@ -66,6 +85,82 @@ int main(int argc, char** argv) {
 	recvlen = recvfrom(_socket, &fileSize, sizeof(off_t), 0, (struct sockaddr*) & address, &addr_length);
 	fprintf(stdout, "Size of Video File to be received: %d bytes\n", fileSize);
 
+	
+
+	recvlen = 1;
+	remainingData = fileSize;
+
+	while (remainingData > 0) {
+
+		// setting array of packets to zero
+		memset(packets, 0, sizeof(packets));
+                for (int i = 0; i < 5; i++){
+                        packets[i].size = 0;
+                }
+
+		// Trying to receive 5 UDP segments
+		for (int i = 0; i < length; i++) {
+			recvlen = recvfrom(_socket, &_packet, sizeof(struct packet), 0, (struct sockaddr*) & address, &addr_length );
+                         if (_packet.size == -999){ printf("last packet found\n");length = _packet.seqNum + 1; }
+
+			// Successfully received packet 
+			if (recvlen > 0) {
+
+				// Keeping the right order of packets by index of the array
+				packets[_packet.seqNum] = _packet;
+                               
+                               
+			}
+		}
+
+		// Sending Acknowledgements for the packets received only
+		_acks = 0;
+		for (int i = 0; i < length; i++) {
+                        
+
+			// Creating acks for the packets received
+			if (packets[i].size !=  0) {
+				acks[i].size = -99;
+				acks[i].seqNum = packets[i].seqNum;
+
+				// Sending acks to the client
+				sendlen = sendto(_socket, &acks[i], sizeof(acks[i]), 0, (struct sockaddr*) & address, addr_length);
+				if (sendlen > 0) {
+					_acks++;
+					fprintf(stdout, "Ack sent: %d\n", acks[i].seqNum);
+				}
+			}
+		}
+
+		// Selective Repeat
+		while (_acks < length) {
+			recvlen = recvfrom(_socket, &_packet, sizeof(struct packet), 0, (struct sockaddr*) & address,&addr_length );
+			if (recvlen > 0) {
+				packets[_packet.seqNum] = _packet;
+				_acks++;
+			}
+	
+		}
+		
+                 WRITE:
+		// write data into file
+		for (int i = 0; i < length; i++) {
+			if (packets[i].size != 0 && packets[i].size !=-999)
+			{
+				fprintf(stdout, "Writing packet: %d\n", packets[i].seqNum);
+				write(file, packets[i].data, packets[i].size);
+				receivedData = receivedData + packets[i].size;
+				remainingData = remainingData - packets[i].size;
+			}
+		}
+
+
+		fprintf(stdout, "Total received bytes: %d\nRemaining data: %d bytes\n", receivedData, remainingData);
+
+
+	}
+
+
 	/*
 	// Receiving data
 	for (;;) {
@@ -83,6 +178,7 @@ int main(int argc, char** argv) {
 		}
 	}
       */
+       close(_socket);
        return 0;
 
 
